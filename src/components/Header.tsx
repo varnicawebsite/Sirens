bcc71c690a204d47c927b343f80bc404993fd2ef
varnicabajaj <try.varnica@gmail.com>
2025-09-16 22:40:47 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -33,7 +33,7 @@ const Header = () => {
             <div className="relative">
               <div className="w-10 h-10 bg-vinyl-gradient rounded-full border-2 border-siren-teal flex items-center justify-center animate-vinyl-spin">
                 <img 
-                  src="/public/WhatsApp Image 2025-09-15 at 7,36,43 PM-photoaidcom-cropped.jpeg" 
+                  src="/public/SirensLogo.jpeg" 
                   alt="Sirens Logo" 
                   className="w-6 h-6 object-cover rounded-full"
                 />
